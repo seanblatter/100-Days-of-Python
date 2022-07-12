832f048bc0f769164d83e9fe0a60c875723931a1
seanblatter <63274835+seanblatter@users.noreply.github.com>
2022-07-12 00:40:20 -0700

    Create Flip a Coin(random).py

---
+++ Flip a Coin(random).py
@@ -0,0 +1,8 @@
+import random
+random_integer = random.randint(1,2)
+random_float = random.random
+
+if random_integer == int("1"):
+    print("Your answer is Heads")
+else:
+    print("Your answer is Tails")
